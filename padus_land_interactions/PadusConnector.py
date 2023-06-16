760a5a10a21d04a2861e39335c440e94ea80fbb3
Jamie Perkins <japrkns@gmail.com>
2023-06-15 20:11:59 -0700

    add getIntersectionArea method

--- padus_land_interactions/PadusConnector.py
+++ padus_land_interactions/PadusConnector.py
@@ -1,9 +1,9 @@
 import sys
 
-from arcgis.geometry import Polygon
+from arcgis.features import FeatureSet
+from arcgis.geometry import Polygon, intersect
 from arcgis.geometry.filters import intersects
 from arcgis.gis import GIS
-from arcgis.features import FeatureSet
 
 # manager type layer: https://services.arcgis.com/v01gqwM5QqNysAAi/ArcGIS/rest/services/Manager_Type/FeatureServer
 
@@ -11,7 +11,6 @@ from arcgis.features import FeatureSet
 # Class for interfacing with the PADUS dataset hosted on ArcGIS online
 class PadusConnector(object):
     def __init__(self) -> None:
-
         # Item ID points to ArcGIS online layer
         MANAGER_TYPE_ITEM_ID = "f0c68c83c88a46dcbb80fd33780ee9f5"
 
@@ -22,6 +21,18 @@ class PadusConnector(object):
     def queryPadusIntersection(self, area: Polygon) -> FeatureSet:
         geom_filter = intersects(area)
         return self.manager_type_layer.query(geometry_filter=geom_filter)
+
+    # Return intersection polygon in desired srid
+    def getIntersectionArea(
+        self, area1: Polygon, area2: Polygon, srid: int = 3857
+    ) -> Polygon:
+        area1Transformed = area1.project_as(srid)  # type: ignore
+        area2Transformed = area2.project_as(srid)  # type: ignore
+
+        intersection_result = intersect(srid, [area2Transformed], area1Transformed)  # type: ignore
+        intersection_area = intersection_result[0]  # type: ignore
+
+        return intersection_area
 
 
 if __name__ == "__main__":
--- test/test_PadusConnector.py
+++ test/test_PadusConnector.py
@@ -12,4 +12,17 @@ def test_sample_query() -> None:
     padus = PadusConnector()
     query_result = padus.queryPadusIntersection(test_area)
 
-    assert len(query_result.features) == 2 # type: ignore
+    assert len(query_result.features) == 2  # type: ignore
+
+
+def test_getIntersectionArea() -> None:
+    test_wkt_string = "Polygon ((-121.71614033828271317 37.30714743764794861, -121.71589013202380158 37.301773880403438, -121.7049748839786929 37.30182363732431838, -121.71614033828271317 37.30714743764794861))"
+    test_area = Polygon().from_shapely(wkt.loads(test_wkt_string))
+
+    padus = PadusConnector()
+    query_result = padus.queryPadusIntersection(test_area)
+    feature_list = query_result.features
+    test_area2 = Polygon(feature_list[0].geometry)
+    intersection_area = padus.getIntersectionArea(test_area, test_area2).area
+
+    assert isinstance(intersection_area, float) and intersection_area > 0
